353febd873e286ff3604cdf4b6d2fa32af744518
Matheus Valle <matheusvalle_vni@outlook.com>
2021-08-26 01:31:16 -0300

    Finish project Tinder Clone

--- src/components/TinderCards/index.js
+++ src/components/TinderCards/index.js
@@ -1,39 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TinderCard from 'react-tinder-card';
+import axios from '../../services/axios';
 
 import "./styles.scss";
 
 export default function TinderCards() {
-  const [people, setPeople] = useState([
-    {
-      name: "Elon Musk",
-      url: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTc5OTk2ODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg",
-    },
-    {
-      name: "Zoë Kravitz",
-      url: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTgyNzU1OTYwMDYwNjUwODQ4/gettyimages-1200246871.jpg",
-    },
-    {
-      name: "Florence Pugh",
-      url: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTgyNzI5MTA4MTkzNTUxNzc5/gettyimages-1205209881.jpg",
-    },
-    {
-      name: "Cleve Jones",
-      url: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTgxNDUxNTcwMTcxMzU3MTMx/cleve-jones_500x500_gettyimages-643277430.jpg",
-    },
-    {
-      name: "Daniel Kaluuya",
-      url: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTc4ODMxODY1MDY5NzA4Nzk5/daniel-kaluuya_500x500_gettyimages-926817010.jpg",
-    },
-    {
-      name: "Amy Coney Barrett",
-      url: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTc1NzIzNDcwNTM2MDU3OTEx/amy_barrett_headshot_university_of_notre_dame_500x0500.jpg",
-    },
-    {
-      name: "Mark Zuckerberg",
-      url: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQyMDA0NDgwMzUzNzcyNjA2/mark-zuckerberg_gettyimages-512304736jpg.jpg",
-    },
-  ]);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get('/tinder/cards');
+
+      setPeople(req.data);
+    }
+
+    fetchData();
+  }, [])
 
   const swiped = (direction, nameToDelete) => {
     console.log("removing: " + nameToDelete);
@@ -56,7 +38,7 @@ export default function TinderCards() {
             onCardLeftScreen={() => outOfFrame(person.name)}
           >
             <div
-              style={{ backgroundImage: `url(${person.url})` }}
+              style={{ backgroundImage: `url(${person.imgUrl})` }}
               className="card"
             >
               <h3>{person.name}</h3>
